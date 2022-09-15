042c6ad3b06c08f89246136ab0bd6b10d2149a7a
tedbeast <theodore.balashov@revature.com>
2022-09-15 13:04:38 -0400

    updated build gradle

--- src/main/java/MuseumAPI.java
+++ src/main/java/MuseumAPI.java
@@ -10,21 +10,22 @@ import java.util.Scanner;
 
 public class MuseumAPI {
     public static void main(String[] args) throws SQLException {
+
         PaintingService ps = new PaintingService();
         ArtistService as = new ArtistService();
         Javalin app = Javalin.create(JavalinConfig::enableCorsForAllOrigins);
 
         app.start(9000);
 
-        app.get("/paintings/", ctx -> {ctx.json(ps.getAllPaintings());});
+        app.get("/paintings/", ctx -> ctx.json(ps.getAllPaintings()));
         app.get("/paintings/artistname/{name}", ctx ->
-        {
+
 //            we're passing in the expected behavior to the web request into the method get
 //            ctx.result returns a http response
 //            ctx.pathParam grabs the param surrounded by curly braces in app.get
 //            for our http response, return the result of paintingservice getallpaintings(name) as a string
-            ctx.json(ps.getAllPaintingsByArtistName(ctx.pathParam("name")));
-        });
+            ctx.json(ps.getAllPaintingsByArtistName(ctx.pathParam("name")))
+        );
         app.get("paintings/artistid/{id}", ctx -> {
             ctx.json(ps.getAllPaintingsByArtistID(Integer.parseInt(ctx.pathParam("id"))));
         });
